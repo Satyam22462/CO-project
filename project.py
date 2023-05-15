50bdf4b7e85fa0eb6a99973385782f3289d2ee5a
Satyam <119166910+Satyam22462@users.noreply.github.com>
2023-05-15 21:43:29 +0530

    Update project.py

--- project.py
+++ project.py
@@ -96,3 +96,33 @@ def function4(y,dopcode,dreg,l,counting):
         error_a="error in line:"+str(counting)+"Typos in register name"
         file2.write(error_a)
         file2.write("\n")
+def function5(y,dopcode,dreg,l,counting):
+    s1=dopcode[y[0]]
+    if(y[1] in dreg and y[1] != "FLAGS" and y[2] in dreg and y[2] != "FLAGS"):
+
+        s2=dreg[y[1]]
+        s3=dreg[y[2]]
+        
+        s5=16-(len(s1)+len(s2)+len(s3))
+        s6=''
+        if(s5!=0):
+            for i in range(0,s5):
+                s6=s6+'0'
+        s7=s1+s6+s2+s3
+        l.append(s7)
+    elif(y[1] not in dreg):
+        error_a="error in line:"+str(counting)+"Typos in register name"
+        file2.write(error_a)
+        file2.write("\n")
+    elif(y[1] == "FLAGS"):
+        error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
+        file2.write(error_a)
+        file2.write("\n")
+    elif(y[2] not in dreg):
+        error_a="error in line:"+str(counting)+"Typos in register name"
+        file2.write(error_a)
+        file2.write("\n")
+    elif(y[2] == "FLAGS"):
+        error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
+        file2.write(error_a)
+        file2.write("\n")
