5e0a7e198a7e431911fbdece2ffcb56d2f8f9aac
Satyam <119166910+Satyam22462@users.noreply.github.com>
2023-05-17 11:53:19 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,39 +1,33 @@
-file=open("input.txt","r")
-file2=open("output.txt",'w')
-
-
-
-
 def function1(y,dopcode,dreg,l,counting):
     s1=dopcode[y[0]]
     if(y[1] == "FLAGS"):
         error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     elif(y[1] not in dreg):
         error_a="error in line:"+str(counting)+"Typos in register name"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     elif(y[2] == "FLAGS"):
         error_a ="error in line:"+str(counting)+ "Illegal use of FLAGS register"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     elif(y[2] not in dreg):
         error_a="error in line:"+str(counting)+"Typos in register name"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     elif(y[1] == "FLAGS"):
         error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     elif(y[1] not in dreg):
         error_a="error in line:"+str(counting)+"Typos in register name"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     # if(y[1] == 'FLAGS' or y[2] == "FLAGS" or y[3] == "FLAGS"):
     #     error_a = "Illegal use of FLAGS register"
-    #     file2.write(error_a)
-    #     file2.write("\n")
+    #     print(error_a)
+    #     print("\n")
     else:
         s2=dreg[y[1]]
         s3=dreg[y[2]]
@@ -64,8 +58,8 @@ def function4(y,dopcode,dreg,l,counting):
             if(address2>=0):
                 if(len(string)>7):
                     error_e="error in line:"+str(counting)+"Illegal immediate value"
-                    file2.write(error_e)
-                    file2.write("\n")
+                    print(error_e)
+                    print("\n")
                 else:
                     s5=7-len(string)
                 
@@ -82,23 +76,23 @@ def function4(y,dopcode,dreg,l,counting):
                     l.append(s8)
             else:
                 error_e="error in line:"+str(counting)+"Illegal immediate value"
-                file2.write(error_e)
-                file2.write("\n")
+                print(error_e)
+                print("\n")
         else:
             q2="error in line:"+str(counting)+"Illegal immediate value"
-            file2.write(error_e)
-            file2.write("\n")
+            print(error_e)
+            print("\n")
     elif(y[1] == "FLAGS"):
         error_a ="error in line:"+str(counting)+ "Illegal use of FLAGS register"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     else:
         error_a="error in line:"+str(counting)+"Typos in register name"
-        file2.write(error_a)
-        file2.write("\n")
-        
-        
-        
+        print(error_a)
+        print("\n")
+
+
+
 def function5(y,dopcode,dreg,l,counting):
     s1=dopcode[y[0]]
     if(y[1] in dreg and y[1] != "FLAGS" and y[2] in dreg and y[2] != "FLAGS"):
@@ -115,21 +109,21 @@ def function5(y,dopcode,dreg,l,counting):
         l.append(s7)
     elif(y[1] not in dreg):
         error_a="error in line:"+str(counting)+"Typos in register name"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     elif(y[1] == "FLAGS"):
         error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     elif(y[2] not in dreg):
         error_a="error in line:"+str(counting)+"Typos in register name"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
     elif(y[2] == "FLAGS"):
         error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
-        file2.write(error_a)
-        file2.write("\n")
-        
+        print(error_a)
+        print("\n")
+
 def function7(y,dopcode,l):
     s1=dopcode[y[0]]
     s2=16-len(s1)
@@ -138,12 +132,12 @@ def function7(y,dopcode,l):
         for i in range(0,s2):
             s3=s3+'0'
     s4=s1+s3
-    l.append(s4)
-    
+    l.append(s4)    
+
 def address(address):
     string = ""
     string2 = ""
-    while address == True or address != 0:
+    while address == True  or address != 0:
         yo = str(address%2)
         string = yo + string
         address = address//2
@@ -165,17 +159,17 @@ def function6(y,dopcode,dict_of_labels,var,l,f,counting):#label wala
             break
     if (flag == 0):
         error_a="error in line:"+str(counting)+"misuse of variable as label"#change
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
+        #f.write("\n")
     elif(e not in dict_of_labels):
         error_a="error in line:"+str(counting)+"Use of undefined labels"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
+        #f.write("\n")
     else:
         a = yo+"0000"+address(dict_of_labels[e])
         l.append(a)
         
-   def loadandstore(y,dopcode,dreg,var,dict_of_labels,dict_of_instructions,l,f,counting):
+def loadandstore(y,dopcode,dreg,var,dict_of_labels,dict_of_instructions,l,f,counting):
     yo = dopcode[y[0]]
     flag = -1
     for i in range(len(var)):
@@ -185,20 +179,16 @@ def function6(y,dopcode,dict_of_labels,var,l,f,counting):#label wala
             break
     if(y[1] == 'FLAGS'):
         error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     elif(y[1] not in dreg):
         error_a = "error in line:"+str(counting)+"Typos in register name"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     elif(y[2] in dict_of_labels):
         error_a="error in line:"+str(counting)+"Misuse of label as variable"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     elif (flag == -1):
         error_a="error in line:"+str(counting)+"Use of undefined variables"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     else:
         c = 0
         for i in dict_of_instructions:
@@ -214,20 +204,17 @@ def mov2(y,dopcode,dreg,l,f,counting):
     yo = dopcode['mov1']
     if(y[1] == 'FLAGS'):
         error_a ="error in line:"+str(counting)+ "Illegal use of FLAGS register"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     if(y[1] not in dreg):
         error_a = "error in line:"+str(counting)+"Typos in register name"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     # elif(y[2] == 'FLAGS'):
     #     error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
     #     f.write(error_a)
     #     f.write("\n")
     elif(y[2] not in dreg):
         error_a = "error in line:"+str(counting)+"Typos in register name"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     else:
         a = yo+"00000"+dreg[y[1]]+dreg[y[2]]
         l.append(a)
@@ -237,16 +224,13 @@ def mov1(y,dopcode,dreg,l,f,counting):
     b = int(c[1:])
     if(y[1] == 'FLAGS'):
         error_a = "error in line:"+str(counting)+"Illegal use of FLAGS register"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     elif(y[1] not in dreg):
         error_a ="error in line:"+str(counting)+ "Typos in register name"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     elif(b<0 or b>127):
         error_a ="error in line:"+str(counting)+ "Illegal Immediate values"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     else:
         a = yo+"0"+dreg[y[1]]+address(b)
         l.append(a)
@@ -256,22 +240,31 @@ def movfinal(y,dopcode,dreg,l,f,counting):
         mov1(y,dopcode,dreg,l,f,counting)
     elif(b.isdigit()):
         error_a = "error in line:"+str(counting)+f" {b} in not defined"
-        f.write(error_a)
-        f.write("\n")
+        f.append(error_a)
     else:
         mov2(y,dopcode,dreg,l,f,counting)
 #dictionary of opcode
 dopcode ={'add':'00000', 'sub':'00001', 'mov':'00010', 'mov1':'00011', 'ld':'00100', 'st':'00101', 'mul':'00110', 'div':'00111', 'rs':'01000', 'ls':'01001', 'xor': '01010', 'or':'01011', 'and':'01011', 'not':'01101', 'cmp':'01110', 'jmp':'01111', 'jlt':'11100', 'jgt':'11101', 'je':'11111', 'hlt':'11010'}
 #dictionary of registers
 dreg = {'R0' :'000','R1' :'001','R2' :'010','R3' :'011','R4' :'100','R5' :'101','R6': '110', 'FLAGS':'111'}
-
+#file2 is ans
+file2=[]
 
 
 
 
 
 #  reads a list of strings, each item being a line
-duplicate_list_of_instructions=file.readlines()
+# duplicate_list_of_instructions=file.readlines()
+
+duplicate_list_of_instructions=[]
+while True:
+    try:
+        testline=input()
+        duplicate_list_of_instructions.append(testline)
+    except EOFError:
+        break
+
 flag1 = -1
 flag2 = -1
 for a in duplicate_list_of_instructions:
@@ -283,14 +276,14 @@ if(flag1 == 0 and flag2 == 0):
     pass
 elif(flag1 == -1 and flag2 == 1): 
     error = "variables not declared at the beginning\n"
-    file2.write(error)
+    print(error)
 else:
     pass
 # a = duplicate_list_of_instructions[0]
 # b = a.split()
 # if(len(b) != 0 and b[0] != "var"):
 #     error = "variables not declared at the beginning\n"
-#     file2.write(error)
+#     print(error)
 list_of_instructions=[]#list without any new line 
 for i in range(len(duplicate_list_of_instructions)):
 
@@ -306,8 +299,8 @@ for i in range(len(duplicate_list_of_instructions)):
 
 # if(l1[0]!='var'):
 #     error_g="Variables not declared at the beginning"
-#     file2.write(error_g)
-#     file2.write("\n")
+#     print(error_g)
+#     print("\n")
 
 
 
@@ -367,10 +360,10 @@ for x,y in dict_of_instructions.items():
             counting=counting+1
         else:#
             q1="error in line:"+str(counting)+"general syntax error"#
-            file2.write(q1)#
-            file2.write("\n") #
+            print(q1)#
+            print("\n") #
             counting=counting+1
-            
+
     elif(y[0]=="ld" or y[0]=="st"):
         if(len(y)==3):#
             loadandstore(y,dopcode,dreg,var,dict_of_labels,dict_of_instructions,l,file2,counting)
@@ -378,8 +371,8 @@ for x,y in dict_of_instructions.items():
             
         else:#
             q1="error in line:"+str(counting)+"general syntax error"#
-            file2.write(q1)#
-            file2.write("\n") #
+            print(q1)#
+            print("\n") #
             counting=counting+1
 
     elif(y[0]=="mov"):
@@ -389,10 +382,10 @@ for x,y in dict_of_instructions.items():
             
         else:#
             q1="error in line:"+str(counting)+"general syntax error"#
-            file2.write(q1)#
-            file2.write("\n") #
+            print(q1)#
+            print("\n") #
             counting=counting+1
-            
+
     elif(y[0]=="ls" or y[0]=="rs"):
         if(len(y)==3):#
             function4(y,dopcode,dreg,l,counting)
@@ -400,10 +393,10 @@ for x,y in dict_of_instructions.items():
             
         else:#
             q1="error in line:"+str(counting)+"general syntax error"#
-            file2.write(q1)#
-            file2.write("\n") #
+            print(q1)#
+            print("\n") #
             counting=counting+1
-            
+
     elif(y[0]=="cmp"or y[0]=="div" or y[0]=="not"):
         
         if(len(y)==3):#
@@ -412,10 +405,10 @@ for x,y in dict_of_instructions.items():
             
         else:#
             q1="error in line:"+str(counting)+"general syntax error"#
-            file2.write(q1)#
-            file2.write("\n") #
+            print(q1)#
+            print("\n") #
             counting=counting+1
-            
+
     elif(y[0]=="jlt"or y[0]=="jmp" or y[0]=="jgt" or y[0]=="je"):
         if(len(y)==2):#
             function6(y,dopcode,dict_of_labels,var,l,file2,counting)
@@ -423,15 +416,14 @@ for x,y in dict_of_instructions.items():
             
         else:#
             q1="error in line:"+str(counting)+"general syntax error"#
-            file2.write(q1)#
-            file2.write("\n") #
+            print(q1)#
+            print("\n") #
             counting=counting+1
-            
+
     elif(y[0]=="hlt"):
         function7(y,dopcode,l)
         variable=x+1
         # print(variable)
-        
         counting=counting+1
         break
     elif(y[0]=="var"):#
@@ -439,14 +431,15 @@ for x,y in dict_of_instructions.items():
 
     else:
         error_a="error in line:"+str(counting)+"Typos in instruction name"
-        file2.write(error_a)
-        file2.write("\n")
+        print(error_a)
+        print("\n")
         counting=counting+1
+  
 variable1=dict_of_instructions.values()                        
-if (['hlt'] not in variable1):
+if (['hlt'] not in variable1 and (len(file2) !=0)):
     error_h="Missing hlt instruction"
-    file2.write(error_h)
-    file2.write("\n")
+    print(error_h)
+    print("\n")
 
 else:
     if(variable<len(dict_of_instructions)):
@@ -454,17 +447,26 @@ else:
         variable2=dict_of_instructions[variable]
         if(variable2[0]=="add" or variable2[0]=="sub" or variable2[0]=="mul" or variable2[0]=="xor" or variable2[0]=="or" or variable2[0]=="and" or variable2[0]=="ld" or variable2[0]=="st" or variable2[0]=="mov" or variable2[0]=="ls" or variable2[0]=="rs" or variable2[0]=="cmp"or variable2[0]=="div" or variable2[0]=="not" or variable2[0]=="jlt" or variable2[0]=="jmp" or variable2[0]=="jgt" or variable2[0]=="je" or len(dict_of_instructions)-variable!=len(var)):
             error_i="error in line:"+str(counting)+"hlt not being used as the last instruction"
-            file2.write(error_i)
-            file2.write("\n")
+            print(error_i)
+            print("\n") 
 # print(l)
-file.close()
-file2.close()
-finished_file1=open("output.txt","r")
-listing=finished_file1.readlines()
-finished_file1.close()
-finished_file=open("output.txt","a")
-if(listing==[]):
-    for m in l:
-        finished_file.write(m)
-        finished_file.write("\n")
-finished_file.close()
+# for i in range(0,len(file2)):
+#     print(file2[i])
+#     print("\n")
+if(len(file2) != 0):
+    for i in range(0,len(file2)):
+        print(file2[i])
+        print("\n")
+else:
+    for i in l:
+        print(i)
+        print("\n")
+# finished_file1=open("output.txt","r")
+# listing=finished_file1.readlines()
+# finished_file1.close()
+# finished_file=open("output.txt","a")
+# if(listing==[]):
+#     for m in l:
+#         finished_file.write(m)
+#         finished_file.write("\n")
+# finished_file.close()
